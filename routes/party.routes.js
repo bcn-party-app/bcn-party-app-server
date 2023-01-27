489c36fa40ce37acfd19836dd95fb2207b04abb8
giulia-colombo <giulia.colombo.127@gmail.com>
2023-01-27 11:11:35 +0100

    worked on party routes

--- routes/party.routes.js
+++ routes/party.routes.js
@@ -5,13 +5,12 @@ const User = require("../models/User.model")
 const Club = require("../models/Club.model")
 
 //POST /api/party - creates a new party
-router.post("/party", (req, res, next) => {
+router.post("/party", async (req, res, next) => {
     const {name, clubId, date, musicGenre, image} = req.body;
 
-    Party.create({name, club: clubId, date, musicGenre, image, attendees: []})
-    .then(newParty => {
-        return Club.findByIdAndUpdate(clubId, { $push: { parties:newParty._id } });
-    })
+    const newParty = await Party.create({name, club: clubId, date, musicGenre, image, attendees: []})
+    Club.findByIdAndUpdate(clubId, { $push: { parties:newParty._id } })
+    .then(() => res.json(newParty))
     .catch(err => res.json(err));
 })
 
@@ -24,36 +23,49 @@ router.get("/party", (req, res, next) => {
 });
 
 //PUT /api/party/:partyId - update a party by Id
-router.put("/party/:partyId", (req, res, next) => {
+router.put("/party/:partyId", async (req, res, next) => {
     const {partyId} = req.params;
-    const {name, club, musicGenre, image} = req.body;
-
-
+    const {name, club, date, musicGenre, image} = req.body;
 
     if (!mongoose.Types.ObjectId.isValid(partyId)) {
         res.status(400).json({ message: 'Specified id is not valid' });
         return;
     }
-    Party.findById(partyId)
-    .then(foundParty => {
-        //remove the party from the current club (remove from parties array)
-        return Club.findByIdAndUpdate(foundParty.club, { $pull: {parties:foundParty._id }})
-        .then()
-    })
 
-    //in the Club model
-        //remove the party from the parties array of the old club
-        //take the clubId of the new club (this will come from req.body)
-        //push the party into the parties array of the new club
+//1. find the party by id (without updating the party)
+//get the id of current club
+// remove the party from current Club parties array
+//update the Party with findbyIdAndUpdate 
 
-    //get the clubId of the current club the party is at (this we already have, because it's in the Party)
-    
-    //in the Party model
-    //update the club name and all the other details (req.body) - DONE
+    const foundParty = await Party.findById(partyId) 
+    console.log("foundParty ==> ", foundParty);
+    Club.findById(foundParty.club, {$pull: {parties:foundParty._id }})
+    .then(() => {
+            return Party.findByIdAndUpdate(partyId, {name, club, musicGenre, image}, {new:true})
+            .then((updatedParty) => res.json(updatedParty))
+        })
 
-    Party.findByIdAndUpdate(partyId, {name, club: clubId, musicGenre, image}, {new:true})
-    .then(updatedParty => res.json(updatedParty))
     .catch(err => res.json(err));
+
+    // Party.findByIdAndUpdate(partyId, {name, club, musicGenre, image}, {new:true})
+    // .then(updatedParty => {
+    //     Club.find({foundParty.club}, { $pull: {parties:foundParty._id }})
+    // }
+
+    // .catch(err => res.json(err));
+
+    // Party.findById(partyId)
+    // .then(foundParty => {
+    //     //remove the party from the current club (remove from parties array)
+    //     return Club.findByIdAndUpdate(foundParty.club, { $pull: {parties:foundParty._id }})
+    // })
+    // .catch(err => res.json(err));
+
+    // Party.findById(partyId)
+    // .then(foundParty => {
+    //     return Club.findByIdAndUpdate(club, {$push: {parties:foundParty._id}})
+    // });
+
 });
 
 //DELETE /api/party/:partyId - 
